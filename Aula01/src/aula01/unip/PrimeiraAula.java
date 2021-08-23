e2c74dd7f37dd98d779ce14f872cca9d4947a078
Sabrina Maldaner <sabrinasp1982@hotmail.com>
2021-08-23 20:28:15 -0300

    initial commit

---
+++ Aula01/src/aula01/unip/PrimeiraAula.java
@@ -0,0 +1,12 @@
+package aula01.unip;
+
+public class PrimeiraAula {
+
+	public static void main(String[] args) {
+		// comments
+		
+		System.out.println("Sandra teacher´s first class");
+
+	}
+
+}
